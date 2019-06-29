315b75b9b303950eeb70e8b1a14c1820acb89291
pipusana kumvongdee <napipusana@hotmail.com>
2019-06-30 02:37:53 +0700

    final v.1

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,75 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      clipped
+      fixed
+      app
+    >
+      <v-list dense>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>dashboard</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Search Restaurants</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <!--<v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>settings</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Settings</v-list-tile-title>
+          </v-list-tile-content>-->
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar app fixed clipped-left color="indigo" dark fixed app>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>Test SCG</v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+        <div id="layoutbody">
+        <router-view />
+      </div>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data: () => ({
+      drawer: null
+    }),
+    props: {
+      source: String
+    }
 }
+
 </script>
 
 <style>
+  #layoutbody {
+    margin: 1%;
+  }
+
+  #layoutsearch{
+    margin: 3%;
+  }
+
+  input[type=text], select {
+  width: 100%;
+  padding: 5px 20px;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+
+<style>
+@import '~vuetify/src/stylus/main'
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/SearchRestaurants.vue
@@ -0,0 +1,150 @@
+<template>
+  <section>
+    <v-card>
+      <v-card-title class="headline font-weight-regular blue-grey white--text">Search</v-card-title>
+      <v-card-text>
+        <vue-google-autocomplete
+            ref="address"
+            id="map"
+            v-model="selectedAddress"
+            classname="form-control"
+            placeholder="Please type your address"
+            v-on:placechanged="getAddressData => selectedAddress = getAddressData"
+            country="TH"
+            text="Bang Sue, Rai Khing, Sam Phran District, Nakhon Pathom, Thailand"
+        >
+        </vue-google-autocomplete>
+        <div>
+          <v-btn small @click="clicklalong">Search</v-btn>
+      </div>
+      </v-card-text>
+    </v-card>
+    <br>
+    <v-card>
+      <v-card-title class="headline font-weight-regular blue-grey white--text">Result</v-card-title>
+      <v-card-text>
+         <table id="customers">
+          <thead>
+            <tr>
+              <th>ชื่อร้าน</th>
+              <th>สถานีที่ใกล้เคียง</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="row in places.data">
+              <td>{{row.name}}</td>
+              <td>{{row.vicinity}}</td>
+            </tr>
+          </tbody>
+        </table>
+
+      </v-card-text>
+    </v-card>
+    <!-- <div>{{ places }}</div> -->
+     {{selectedAddress.latitude+','+selectedAddress.longitude }} 
+  </section>
+</template>
+
+<script>
+import VueGoogleAutocomplete from 'vue-google-autocomplete'
+// import { mapGetters, mapActions } from 'vuex'
+import axios from 'axios'
+const helper = {
+    BASE_API: `http://localhost:5000`
+}
+export default {
+  components: { VueGoogleAutocomplete },
+  name: 'HelloWorld',
+  data () {
+    return {
+        address: ''
+        ,selectedAddress: 'Bang Sue, Bangkok'
+        ,places: []
+        ,lalong: ''
+        ,location: '13.82220,100.525507'
+    }
+  },
+  watch: {
+    selectedAddress: function (val) {
+      let latitude = Number.parseFloat(val.latitude).toFixed(5)
+      let longitude = Number.parseFloat(val.longitude).toFixed(5)
+      this.location = latitude + ',' + longitude.toString()
+    }
+  },
+  // computed: {
+  //   ...mapGetters('getPlaces')
+  // },
+  created () {
+    this.hello = this.getHelloWroldObject
+    // this.places = this.getGooglePlacesAction('13.7473366,100.537578')
+  },
+  mounted() {
+            // To demonstrate functionality of exposed component functions
+            // Here we make focus on the user input
+            this.$refs.address.focus();
+            this.getGooglePlacesAction(this.location)
+        },
+  methods: {
+    // ...mapActions('getGooglePlacesAction'),
+    getAddressData: function (addressData, placeResultData, id, latitude, longitude) {
+                 this.address = addressData;     
+    },
+    async getGooglePlacesAction(location) {
+      await axios({
+        url: `${helper.BASE_API}/google/places?location=${location}`,
+        method: 'GET'
+      }).then(response => {
+        if(response.status) {
+          this.places = response.data
+          // console.log(response.data)
+        }
+      }).catch( err => { console.log(err) })
+    },
+    clicklalong: function(){
+    this.getGooglePlacesAction(this.location)
+    }
+  }
+
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+#customers {
+  font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+#customers td, #customers th {
+  border: 1px solid #ddd;
+  padding: 8px;
+}
+
+#customers tr:nth-child(even){background-color: #f2f2f2;}
+
+#customers tr:hover {background-color: #ddd;}
+
+#customers th {
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: left;
+  background-color: #4CAF50;
+  color: white;
+}
+</style>
